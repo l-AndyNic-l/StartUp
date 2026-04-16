48f6a5b9b0ddcaafb77f226e857c4e372dfd622f
Adrian Lopez <adri.lopez@duocuc.cl>
2026-04-16 16:15:31 -0400

    Inicio del Proyecto

---
+++ start_up/src/main/java/cl/duoc/start_up/StartUpApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.start_up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpApplication.class, args);
+	}
+
+}
---
+++ start_up/src/test/java/cl/duoc/start_up/StartUpApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.start_up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
